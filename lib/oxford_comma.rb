c84aee3a357f413ac78ad9034b21e3b76d7ebfd1
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 17:28:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 2 
    return array.join(" and ")
+   elsif array.length == 1
+   return array.join
  else array.length > 2 
    array[-1].insert(0, "and ")
    return array.join(", ")
