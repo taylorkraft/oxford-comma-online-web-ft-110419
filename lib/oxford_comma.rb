5da78d28db5ab80af729b9c37fdfe667363738c4
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 16:48:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,4 @@ def oxford_comma(array)
   end
 end
 
-def oxford_comma(array)
-  if array.length == 3
-    puts array.join(", ", " and ")
-  else array.length > 3 
-    puts array.join << element
-  end
-end
+
